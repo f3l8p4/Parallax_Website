e344d3233503e6546e9544ee5e7d1531e3b2dfa3
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2021-08-30 21:20:52 -0300

    config

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ let moon = document.getElementById('moon')
 let stars = document.getElementById('stars')
 let mountains_behind = document.getElementById('mountains_behind')
 let mountains_front = document.getElementById('mountains_front')
-let button = document.getElementsByClassName('btn') 
-let text = document.getElementsByClassName('text')
-
+let button = document.getElementById('btn') 
+let text = document.getElementById('text')
+let header = document.querySelector('header')
 window.addEventListener('scroll',function(){
 let value = window.scrollY
 stars.style.left = value * 0.025 +'px'
 moon.style.top = value * 1.05 + 'px'
-mountains_behind.style.top = value + 0.5 + 'px'
-mountains_front.style.top = value + 0.5 + 'px'
-text.style.marginRight = value + 4 + 'px'
+mountains_behind.style.top = value * 0.5 + 'px'
+mountains_front.style.top = value * 0 + 'px'
+text.style.marginRight = value * 2 + 'px'
+text.style.marginTop = value * 1.5 + 'px'
+button.style.marginTop = value * 1.5 + 'px'
+header.style.top = value * 0.5 + 'px'
 })
-console.log('Ola')
-console.log('hola que ta')
